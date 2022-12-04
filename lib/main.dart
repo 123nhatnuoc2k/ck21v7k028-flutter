650f86f8907574bbd387626395fb72ddd3512ee5
Phạm Đức Tài <66506120+123nhatnuoc2k@users.noreply.github.com>
2022-12-04 20:25:07 +0700

    Xay dung trang hieu chinh san pham

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:myshop/ui/products/edit_product_screen.dart';
 import 'ui/screens.dart';
 import 'package:provider/provider.dart';
 
-
 void main() {
   runApp(const MyApp());
 }
@@ -15,67 +14,57 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(
-          create: (ctx) => ProducsManager()
-          ),
+        ChangeNotifierProvider(create: (ctx) => ProducsManager()),
         ChangeNotifierProvider(
           create: (ctx) => CartManager(),
         ),
-        ChangeNotifierProvider(
-          create: (ctx) => OrdersManager()
-        )
+        ChangeNotifierProvider(create: (ctx) => OrdersManager())
       ],
       child: MaterialApp(
         // return MaterialApp(
-            title: 'My Shop',
-            debugShowCheckedModeBanner: false,
-            theme: ThemeData(
-              fontFamily: 'Lato',
-              colorScheme: ColorScheme.fromSwatch(
-                primarySwatch: Colors.purple,
-              ).copyWith(
-                secondary: Colors.deepOrange,
-              ),
-            ),
-            home: const ProductsOverviewScreen(),
-            routes: {
-              CartScreen.routeName: 
-                (ctx) => const CartScreen(),
-              OrdersScreen.routeName: 
-                (ctx) => const OrdersScreen(),
-              UserProductsScreen.routeName: 
-                (ctx) => const UserProductsScreen(),
-              },
-              onGenerateRoute: (setting) {
-                if (setting.name == EditProductScreen.routeName) {
-                  final productId = setting.arguments as String?;
-                  return MaterialPageRoute(
-                    builder: (ctx) {
-                      return EditProductScreen(
-                        productId != null
-                        ? ctx.read<ProducsManager>().findById(productId)
-                        : null,
-                      );
-                    },
-                  );
-                }
-
-
-                // if (setting.name == ProductDetailScreen.routeName) {
-                //   final productId = setting.arguments as String;
-                // return MaterialPageRoute(
-                //   builder: (ctx) {
-                //     return ProductDetailScreen(
-                //       ProducsManager().findById(productId)!,
-                //       );
-                //     },
-                //   );
-                // }
-                  return null;
-              },
+        title: 'My Shop',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          fontFamily: 'Lato',
+          colorScheme: ColorScheme.fromSwatch(
+            primarySwatch: Colors.purple,
+          ).copyWith(
+            secondary: Colors.deepOrange,
           ),
-      );
+        ),
+        home: const ProductsOverviewScreen(),
+        routes: {
+          CartScreen.routeName: (ctx) => const CartScreen(),
+          OrdersScreen.routeName: (ctx) => const OrdersScreen(),
+          UserProductsScreen.routeName: (ctx) => const UserProductsScreen(),
+        },
+        onGenerateRoute: (setting) {
+          if (setting.name == EditProductScreen.routeName) {
+            final productId = setting.arguments as String?;
+            return MaterialPageRoute(
+              builder: (ctx) {
+                return EditProductScreen(
+                  productId != null
+                      ? ctx.read<ProducsManager>().findById(productId)
+                      : null,
+                );
+              },
+            );
+          }
+
+          // if (setting.name == ProductDetailScreen.routeName) {
+          //   final productId = setting.arguments as String;
+          // return MaterialPageRoute(
+          //   builder: (ctx) {
+          //     return ProductDetailScreen(
+          //       ProducsManager().findById(productId)!,
+          //       );
+          //     },
+          //   );
+          // }
+          return null;
+        },
+      ),
+    );
   }
-
 }
-
